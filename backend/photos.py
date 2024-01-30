ead010ce810f4e3bc33db44ff81c298c6866e6d8
Eric Huie <erichuie97@gmail.com>
2024-01-30 15:56:22 -0800

    added download photo method

--- backend/photos.py
+++ backend/photos.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import boto3
 # from botocore.exceptions import ClientError
 
-S3_URI = "s3://sharebandb-photos"
+# S3_URI = os.environ['S3_URI']
 TEST_PHOTO_URI = "s3://sharebandb-photos/test-photo.png"
 
 load_dotenv()
@@ -14,35 +14,22 @@ load_dotenv()
 # get session for S3
 S3_ACCESS_KEY = os.environ['S3_ACCESS_KEY']
 S3_SECRET_ACCESS_KEY = os.environ['S3_SECRET_ACCESS_KEY']
-session = boto3.Session(
-    aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_ACCESS_KEY,
+
+s3 = boto3.client(
+  "s3",
+  "us-west-1",
+  aws_access_key_id=S3_ACCESS_KEY,
+  aws_secret_access_key=S3_SECRET_ACCESS_KEY,
 )
 
-s3 = session.resource('s3')
 
 test_file = 'test-photo-2.jpg'
-# with open(test_file) as file:
-# print('file', test_file)
-bucket = "sharebandb-photos"
 
-s3.meta.client.upload_file(
-    Filename=test_file,
-    Bucket=bucket,
-    Key='s3_output_key')
+bucket = os.environ['S3_BUCKET']
 
+# s3.upload_file(test_file,
+#     bucket,
+#     test_file)
 
-# def upload_file(file_name, bucket):
+s3.download_file(bucket, test_file, 'test-photo-2.jpg')
 
-#     # If S3 object_name was not specified, use file_name
-#     object_name = os.path.basename(file_name)
-
-#     # Upload the file
-#     s3_client = boto3.client('s3')
-#     try:
-#         response = s3_client.upload_file(file_name, bucket, object_name)
-#         print("response", response)
-#     except ClientError as e:
-#         print("error", e)
-#         return False
-#     return True
