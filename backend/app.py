f1c88444c4ff76f27dff5e06a52bf7816eaea9d2
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-30 14:14:54 -0800

    added placeholder files

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,17 @@
 """Flask app for sharebandb backend"""
+
+import os
+from dotenv import load_dotenv
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
+
+################################################################################
