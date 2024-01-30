7daffbddd607063712bac42df62663cc39f6a2a3
Eric Huie <erichuie97@gmail.com>
2024-01-30 14:46:13 -0800

    added docstrings for backend routes

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -14,4 +17,56 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 toolbar = DebugToolbarExtension(app)
 
+connect_db(app)
+
 ################################################################################
+
+
+@app.get('/listings')
+def get_listings():
+    """Makes a request to database for details about all listings
+        Returns [ {id, name, description, price},... ]
+    """
+
+@app.get('/listings/<int:listing_id>')
+def get_listing(listing_id):
+    """Makes a request to database for details about a certain listing
+        Returns
+        {
+            id,
+            name,
+            description,
+            price,
+            photos:
+            [{
+                id,
+                description,
+                s3_location
+            }]
+        }
+    """
+
+@app.post('/listings/new')
+def add_listing():
+    """Add a new listing to database
+    Input
+        {
+            name,
+            description,
+            price,
+            photos: [
+                {
+                    file path,
+                    descriptions
+                }
+            ]
+        }
+
+    Return
+    {
+        name,
+        description,
+        price,
+        photos: URL location
+    }
+    """
